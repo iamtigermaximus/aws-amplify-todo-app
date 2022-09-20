2f5f481fbbcdd5d9b90cbf73986f9966d14604d7
Siegfred Gamboa <siegy.gamboa@gmail.com>
2022-09-21 00:53:01 +0300

    configure amplify backend connection

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg'
+import { withAuthenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
+import { API } from 'aws-amplify'
+import React, { useEffect, useState } from 'react'
+import './App.css'
 import {
-  withAuthenticator,
-  Button,
-  Heading,
-  Image,
-  View,
-  Card,
-} from '@aws-amplify/ui-react'
+  createTodo as createTodoMutation,
+  deleteTodo as deleteTodoMutation,
+} from './graphql/mutations'
+import { listTodos } from './graphql/queries'
+
+const initialFormState = { name: '', description: '' }
 
 function App({ signOut }) {
+  const [todos, setTodos] = useState([])
+  const [formData, setFormData] = useState(initialFormState)
+
+  useEffect(() => {
+    fetchTodos()
+  }, [])
+
+  async function fetchTodos() {
+    const apiData = await API.graphql({ query: listTodos })
+    setTodos(apiData.data.listTodos.items)
+  }
+
+  async function createTodo() {
+    if (!formData.name || !formData.description) return
+    await API.graphql({
+      query: createTodoMutation,
+      variables: { input: formData },
+    })
+    setTodos([...todos, formData])
+    setFormData(initialFormState)
+  }
+  async function deleteTodo({ id }) {
+    const newTodosArray = todos.filter((todo) => todo.id !== id)
+    setTodos(newTodosArray)
+    await API.graphql({
+      query: deleteTodoMutation,
+      variables: { input: { id } },
+    })
+  }
   return (
-    <View className='App'>
-      <Card>
-        <Image src={logo} className='App-logo' alt='logo' />
-        <Heading level={1}>We now have Auth!</Heading>
-      </Card>
-      <Button onClick={signOut}>Sign Out</Button>
-    </View>
+    <div className='App'>
+      <h1>My Notes App</h1>
+      <input
+        onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+        placeholder='Note name'
+        value={formData.name}
+      />
+      <input
+        onChange={(e) =>
+          setFormData({ ...formData, description: e.target.value })
+        }
+        placeholder='Note description'
+        value={formData.description}
+      />
+      <button onClick={createTodo}>Create Note</button>
+      <div style={{ marginBottom: 30 }}>
+        {todos.map((todo) => (
+          <div key={todo.id || todo.name}>
+            <h2>{todo.name}</h2>
+            <p>{todo.description}</p>
+            <button onClick={() => deleteTodo(todo)}>Delete todo</button>
+          </div>
+        ))}
+      </div>
+      <button onClick={signOut}>Sign Out</button>
+    </div>
   )
 }
 
